a50743b40db5cc92d7cec17eb7d4b67478354050
Laith Harb <35265876+harblaith7@users.noreply.github.com>
2021-01-23 15:15:40 -0500

    Update data.js

--- Client/assets/data.js
+++ Client/assets/data.js
@@ -5,16 +5,19 @@ export const largeCardSections = [
       snippet: "User say these are the best fire extinguishers to rent if your house is burning down.",
       cards: [
         {
+          id: 19,
           title: "Toronto's Portable 2L Fire Extinguisher",
           snippet: "This fire extinguisher saved homes from completely burning down. People that used it only had 98% of their homes burned down.",
           image: "fe1.jpg"
         },
         {
+          id: 20,
           title: "Empty Super Red Fire Extinguisher",
           snippet: "This is one of the most red fire extinguishers you will ever see. Super clean. It's pretty much useless otherwise.",
           image: "fe2.jpg"
         },
         {
+          id: 21,
           title: "Fire Extinguisher, Perfect for Pools",
           snippet: "If you ever find that your pool is on fire then this is the fire extinguisher for you. It has a 100% success rate.",
           image: "fe3.jpg"
@@ -27,16 +30,19 @@ export const largeCardSections = [
       snippet: "Take down that fire lavishly and in style with our luxury extinguishers.",
       cards: [
         {
+          id: 22,
           title: "Vintage 1864 Fire Extinguisher",
           snippet: "This is hands down the worst fire extinguisher you can use in a crisis. The radiation will kill you if the fire doesn't",
           image: "fe4.png"
         },
         {
+          id: 23,
           title: "Pure Silver & Gold Fire Extinguisher",
           snippet: "This one is made out of 50k worth of pure silver and white gold. The metal choice was a poor for functionality, but was great for style!",
           image: "fe5.png"
         },
         {
+          id: 24,
           title: "Two in One Fire Extinguisher",
           snippet: "This is the first ever two in one fire extinguisher. Good if both you and your friend's houses are burining down",
           image: "fe6.jpg"
