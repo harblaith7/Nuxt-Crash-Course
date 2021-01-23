785512a2cc58d7093160323188764f3676130bce
Laith Harb <35265876+harblaith7@users.noreply.github.com>
2021-01-23 12:51:47 -0500

    completed demo

---
+++ Client/assets/data.js
@@ -0,0 +1,157 @@
+export const largeCardSections = [
+    {
+      id: 1,
+      title: "Top-rated Fire Extinguisher in Niagara",
+      snippet: "User say these are the best fire extinguishers to rent if your house is burning down.",
+      cards: [
+        {
+          title: "Toronto's Portable 2L Fire Extinguisher",
+          snippet: "This fire extinguisher saved homes from completely burning down. People that used it only had 98% of their homes burned down.",
+          image: "fe1.jpg"
+        },
+        {
+          title: "Empty Super Red Fire Extinguisher",
+          snippet: "This is one of the most red fire extinguishers you will ever see. Super clean. It's pretty much useless otherwise.",
+          image: "fe2.jpg"
+        },
+        {
+          title: "Fire Extinguisher, Perfect for Pools",
+          snippet: "If you ever find that your pool is on fire then this is the fire extinguisher for you. It has a 100% success rate.",
+          image: "fe3.jpg"
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: "Luxury Fire Extinguisher",
+      snippet: "Take down that fire lavishly and in style with our luxury extinguishers.",
+      cards: [
+        {
+          title: "Vintage 1864 Fire Extinguisher",
+          snippet: "This is hands down the worst fire extinguisher you can use in a crisis. The radiation will kill you if the fire doesn't",
+          image: "fe4.png"
+        },
+        {
+          title: "Pure Silver & Gold Fire Extinguisher",
+          snippet: "This one is made out of 50k worth of pure silver and white gold. The metal choice was a poor for functionality, but was great for style!",
+          image: "fe5.png"
+        },
+        {
+          title: "Two in One Fire Extinguisher",
+          snippet: "This is the first ever two in one fire extinguisher. Good if both you and your friend's houses are burining down",
+          image: "fe6.jpg"
+        }
+      ]
+    }
+  ]
+
+  export const smallCardSections = [
+      {
+          id: 1,
+          title: "Fire Extinguisher in Toronto",
+          cards: [
+              {
+                id: 1,
+                image: "fe7.jpg"
+              },
+              {
+                id: 2,
+                image: "fe8.jpg"
+              },
+              {
+                id: 3,
+                image: "fe9.jpg"
+              },
+              {
+                id: 4,
+                image: "fe10.jpg"
+              },
+              {
+                id: 5,
+                image: "fe11.jpg"
+              },
+              {
+                id: 6,
+                image: "fe12.jpg"
+              },
+              {
+                id: 7,
+                image: "fe13.jpg"
+              },
+              {
+                id: 8,
+                image: "fe14.jpg"
+              },
+              {
+                id: 9,
+                image: "fe15.jpg"
+              },
+              {
+                id: 10,
+                image: "fe16.jpg"
+              },
+              {
+                id: 11,
+                image: "fe17.jpeg"
+              },
+              {
+                id: 12,
+                image: "fe18.jpg"
+              }
+          ]
+      },
+      {
+        id: 2,
+        title: "Fire Extinguisher in Niagara",
+        cards: [
+            {
+              id: 13,
+              image: "fe19.jpg"
+            },
+            {
+              id: 14,
+              image: "fe20.jpg"
+            },
+            {
+              id: 15,
+              image: "fe21.jpg"
+            },
+            {
+              id: 16,
+              image: "fe22.jpg"
+            },
+            {
+              id: 17,
+              image: "fe23.jpg"
+            },
+            {
+              id: 18,
+              image: "fe24.jpg"
+            },
+            {
+              id: 19,
+              image: "fe1.jpg"
+            },
+            {
+              id: 20,
+              image: "fe2.jpg"
+            },
+            {
+              id: 21,
+              image: "fe3.jpg"
+            },
+            {
+              id: 22,
+              image: "fe4.png"
+            },
+            {
+              id: 23,
+              image: "fe5.png"
+            },
+            {
+              id: 24,
+              image: "fe5.png"
+            }
+        ]
+    }
+  ]
---
+++ Client/components/ReviewCard.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="review-card">
+        <div class="image-container">
+            <img :src="review.picture.large" alt="">
+        </div>
+        <div class="text-container" >
+            <h6>{{review.login.username}}</h6>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus quam magni beatae possimus debitis.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['review'],
+    }
+</script>
+
+<style scoped>
+    .review-card {
+        margin-top: 1.5rem;
+        height: 4rem;
+        display: flex;
+    }
+    .text-container {
+        margin-left: 2rem
+    }
+    img {
+        width: 4rem;
+        height: 4rem;
+        border-radius: 100%;
+    }
+    p {
+        color: grey
+    }
+</style>
